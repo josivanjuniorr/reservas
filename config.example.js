633129e61c52e955aace45c5b58cc1db3192cc45
josivanjuniorr <josivanjuniorr@users.noreply.github.com>
2025-12-06 03:04:41 +0000

    deploy: 6d4272a12202b66de829e033776c135faa877604

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 // Copy this file to `config.js` (do NOT commit `config.js`).
 // Fill the `url` and `anonKey` from your Supabase project settings.
 window.SUPABASE_CONFIG = {
-  url: 'https://ersmikfplbltihhsqzgd.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVyc21pa2ZwbGJsdGloaHNxemdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3MjY4MjEsImV4cCI6MjA4MDMwMjgyMX0.IQNGOzDiij-Qa_sxcl8Y9oug5IyVKo_nRKpFPuwhka4'
+  url: 'https://abcjhhzqyknvgashtpbm.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFiY2poaHpxeWtudmdhc2h0cGJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ5ODM5MTAsImV4cCI6MjA4MDU1OTkxMH0.yl5DPDQrrkh2Ds9UyAMaQGnuoxOZ9B2fAGspJxK4EsI'
 };
